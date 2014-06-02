45e79819aeea5fbe65c58e4f67767113024d2ade
Jesse La Russo <jlarusso@canvas.is>
2014-06-02 17:35:02 -0400

    Add .ruby-version

--- template.rb
+++ template.rb
@@ -2,5 +2,6 @@ require 'fileutils'
 
 # Create Gemfile
 run "rm Gemfile"
-run "cp ../canvas-rails-template/Gemfile ."
+run "cp ../canvas-rails-template/root/Gemfile ."
+run "cp ../canvas-rails-template/root/.ruby-version ."
 
